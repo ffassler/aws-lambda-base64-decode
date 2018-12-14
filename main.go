7ed0bba1b40e4b31da54e32b52605a2fa8dd0774
Fabien Fassler <fabien.fassler@sqits.net>
2018-12-14 15:02:08 +0100

    First implementation

---
+++ main.go
@@ -0,0 +1,15 @@
+package awslambdabase64decode
+
+import (
+	"encoding/base64"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Decode64(s string) (string, error) {
+	value, err := base64.StdEncoding.DecodeString(s)
+	return string(value), err
+}
+
+func main() {
+	lambda.Start(Decode64)
+}
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package awslambdabase64decode
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecode(t *testing.T) {
+	source := "In go we trust"
+	s := base64.StdEncoding.EncodeToString([]byte(source))
+	dest, err := Decode64(s)
+
+	assert.IsType(t, nil, err)
+	assert.Equal(t, source, dest)
+}
+
+func TestDecodeErr(t *testing.T) {
+	_, err := Decode64(string([]byte{0, 1, 2, 3}))
+
+	assert.NotNil(t, err)
+}
